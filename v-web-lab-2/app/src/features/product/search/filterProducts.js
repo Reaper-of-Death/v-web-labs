bb7b68f1b73ad818a3b7f515e755a6d26b2db085
Reaper-of-Death <91005752+Reaper-of-Death@users.noreply.github.com>
2025-10-07 12:32:00 +0300

    Delete v-web-lab-2/app directory

--- v-web-lab-2/app/src/features/product/search/filterProducts.js
+++
@@ -1,9 +0,0 @@
-export const filterProducts = (products, query) => {
-  if (!query) return products;
-  
-  const lowercasedQuery = query.toLowerCase();
-  return products.filter(product =>
-    product.name.toLowerCase().includes(lowercasedQuery) ||
-    product.description?.toLowerCase().includes(lowercasedQuery)
-  );
-}
--- v-web-lab-2/app/src/widgets/layout/footer/index.jsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-	return <footer class="h-[50px] items-center flex p-1.5 border-t border-gray-210">
-		<div class="flex gap-5 items-center justify-center">
-        <p>&copy; 2025 Магазин</p>
-        <p>Политика и безопасность</p>
-        <p>Cookie</p>
-      </div>
-	</footer>;
-};
